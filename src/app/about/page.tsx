c48b0499162eaf22f7388a5761f4d700fbb0bef9
Takeda.m <masaakit@tcs-ipnet.co.jp>
2023-07-11 19:04:40 +0900

    自己紹介ページを追加

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  ustify-between p-24">
+      <div>
+        <h1>自己紹介ぺージ</h1>
+        <p>私の名前は○○です</p>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Hello World!!</h1>
         <p>ようこそ私のブログへ。ゆっくりしていってね！</p>
+        <Link href="/about">自己紹介ぺージ</Link>
       </div>
     </main>
   )
