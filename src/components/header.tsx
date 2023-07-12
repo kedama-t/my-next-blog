b917e99d2123c831479e86858628ba117ed5cea6
Takeda.m <masaakit@tcs-ipnet.co.jp>
2023-07-12 14:20:13 +0900

    daisyUIのインストール、ナビゲーションバーの追加

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="navbar rounded-lg m-4 bg-base-300">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost normal-case text-xl">マイブログ</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/categories/blog">日常ブログ</Link>
+          </li>
+          <li>
+            <Link href="/categories/technology">テックブログ</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
